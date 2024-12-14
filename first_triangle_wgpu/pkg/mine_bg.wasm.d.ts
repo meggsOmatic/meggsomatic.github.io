cacebff7fc980581d2dcec0a2b4bf4702768539b
Andrew Meggs <ameggs@gmail.com>
2024-12-13 23:06:50 -0500

    Add files via upload

---
+++ first_triangle_wgpu/pkg/mine_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdea1912bb69c911a: (a: number, b: number) => void;
+export const closure8_externref_shim: (a: number, b: number, c: any) => void;
+export const closure11_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure1043_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2551_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd93d645a885c64c3: (a: number, b: number) => void;
+export const closure2624_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
